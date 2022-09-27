a12a3d1c328b6c77025f4c4bf182d3ad5097bd8f
bala <bala@finsmartstation.com>
2022-09-27 10:49:43 +0530

    changes 23-09-2022 10.49 AM

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo systemctl restart nginx
 
+sudo chmod -R 777 /var/www/html/new
+npm i
+
 sudo pm2 kill
 sudo pm2 start /var/www/html/new/app.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,5 +4,4 @@ if [[ -n  $isExistApp ]]; then
     service httpd stop        
 fi
 
-# pm2 kill
 sudo systemctl stop nginx
