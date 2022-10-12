b89fe97f07af867037d08d11364a4c7dbbb89cbf
bala <bala@finsmartstation.com>
2022-10-12 11:55:48 +0530

    changes 12-10-2022 12.00 PM

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,5 +4,7 @@ sudo systemctl restart nginx
 sudo chmod -R 777 /var/www/html/new
 npm i
 
-sudo pm2 kill
-sudo pm2 start /var/www/html/new/app.js
+cd /var/www/html/new/
+
+killall -9 node
+nodemon app.js
